7188d79c710adc4fa5ba8df385fcb0cf20320ef2
jtareen <jtareen440@gmail.com>
2024-11-03 03:13:53 +0500

    made some changes

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,10 +1,11 @@
 import 'package:car_renr_app/screens/signup_screen.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/scoial_buttons_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:car_renr_app/utils/styles.dart';
-import 'package:car_renr_app/widgets/Login_divider.dart';
-import 'package:car_renr_app/widgets/password_field.dart';
-import 'package:car_renr_app/widgets/signinup_page_button.dart';
-import 'package:car_renr_app/widgets/signinup_page_textfield.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/Login_divider.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/password_field.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/signinup_page_button.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/signinup_page_textfield.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
@@ -20,53 +21,80 @@ class SignInPage extends StatelessWidget {
             icon: const Icon(Icons.arrow_back_ios_new, color: primary,)
         ),
       ),
-      body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Welcome to, Pikbil',
-              style: TextStyle(
-                color: primary,
-                fontSize: 24,
-                fontWeight: FontWeight.w600
-              ),
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome to, Pikbil',
+                    style: TextStyle(
+                        color: primary,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600
+                    ),
+                  ),
+                  const Text(
+                    'Enter your pikbil account to continue',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  SignInUpPageTextField(label: 'Email Address', hint: 'Your email address', inputType: TextInputType.emailAddress,),
+                  const SizedBox(height: 20,),
+                  PasswordField(),
+                  const SizedBox(height: 20,),
+                  SignInUpPageButton(label: 'Login', onPressed: (){}),
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                          onTap: () {
+                            // on tap functionality
+                          },
+                          child: const Text('Forgot password?', style: TextStyle(color: Colors.grey),)
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20,),
+                  const LoginDivider(label: 'or login with'),
+                  const SizedBox(height: 20,),
+                  const SocialButtonsWidget(),
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Didn't have a pikbil account? ",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 16,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                        },
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(
+                            color: primary, // Adjust color as needed
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]
             ),
-            const Text(
-              'Enter your pikbil account to continue',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16
-              ),
-            ),
-            const SizedBox(height: 20,),
-            SignInUpPageTextField(label: 'Email Address', hint: 'Your email address', inputType: TextInputType.emailAddress,),
-            const SizedBox(height: 20,),
-            PasswordField(),
-            const SizedBox(height: 20,),
-            SignInUpPageButton(label: 'Login', onPressed: (){}),
-            const SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                InkWell(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
-                    },
-                    child: const Text('Forgot password', style: TextStyle(color: Colors.grey),)
-                )
-              ],
-            ),
-            const SizedBox(height: 20,),
-            const LoginDivider(label: 'or login with')
-          ]
-        ),
+          )
       )
     );
   }
 }
-
-
-
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,9 +1,10 @@
+import 'package:car_renr_app/widgets/login_register_widgets/scoial_buttons_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:car_renr_app/utils/styles.dart';
-import 'package:car_renr_app/widgets/Login_divider.dart';
-import 'package:car_renr_app/widgets/password_field.dart';
-import 'package:car_renr_app/widgets/signinup_page_button.dart';
-import 'package:car_renr_app/widgets/signinup_page_textfield.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/Login_divider.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/password_field.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/signinup_page_button.dart';
+import 'package:car_renr_app/widgets/login_register_widgets/signinup_page_textfield.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
@@ -19,38 +20,68 @@ class SignUpPage extends StatelessWidget {
               icon: const Icon(Icons.arrow_back_ios_new, color: primary,)
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Nice to know you!',
-                  style: TextStyle(
-                      color: primary,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Nice to know you!',
+                    style: TextStyle(
+                        color: primary,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600
+                    ),
                   ),
-                ),
-                const Text(
-                  "It's you first time to use pikbil",
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16
+                  const Text(
+                    "It's you first time to use pikbil",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20,),
-                SignInUpPageTextField(label: 'Full name', hint: 'Your full name', inputType: TextInputType.text),
-                const SizedBox(height: 20,),
-                SignInUpPageTextField(label: 'Email Address', hint: 'Your email address', inputType: TextInputType.emailAddress,),
-                const SizedBox(height: 20,),
-                PasswordField(),
-                const SizedBox(height: 20,),
-                SignInUpPageButton(label: 'Register', onPressed: (){}),
-                const SizedBox(height: 20,),
-                const LoginDivider(label: 'or register with')
-              ]
+                  const SizedBox(height: 20,),
+                  SignInUpPageTextField(label: 'Full name', hint: 'Your full name', inputType: TextInputType.text),
+                  const SizedBox(height: 20,),
+                  SignInUpPageTextField(label: 'Email Address', hint: 'Your email address', inputType: TextInputType.emailAddress,),
+                  const SizedBox(height: 20,),
+                  PasswordField(),
+                  const SizedBox(height: 20,),
+                  SignInUpPageButton(label: 'Register', onPressed: (){}),
+                  const SizedBox(height: 20,),
+                  const LoginDivider(label: 'or register with'),
+                  const SizedBox(height: 20,),
+                  const SocialButtonsWidget(),
+                  const SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have a pikbil account? ",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 16,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: primary, // Adjust color as needed
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]
+            ),
           ),
         )
     );
