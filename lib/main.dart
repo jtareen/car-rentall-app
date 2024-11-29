4551fff5dc80c161b6b89b7c2197fc1a9bd23175
jtareen <jtareen440@gmail.com>
2024-11-29 19:39:44 +0500

    minor changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'package:car_renr_app/screens/login/email_verification_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:car_renr_app/firebase_options.dart';
-import 'package:car_renr_app/screens/login/forgot_password_screen.dart';
-import 'package:car_renr_app/screens/main/main_screen.dart';
 import 'package:car_renr_app/screens/onboarding/onboarding_screen.dart';
+import 'package:car_renr_app/screens/login/forgot_password_screen.dart';
 import 'package:car_renr_app/screens/login/otp_verification_screen.dart';
 import 'package:car_renr_app/screens/login/recover_account_screen.dart';
 import 'package:car_renr_app/screens/login/signin_screen.dart';
 import 'package:car_renr_app/screens/login/signup_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:car_renr_app/screens/main/main_screen.dart';
 
 void main() async {
   try {
@@ -35,6 +36,7 @@ class MainApp extends StatelessWidget {
           '/signup' : (context) => const SignUpPage(),
           '/forgotpassword' : (context) => const ForgotPasswordPage(),
           '/otpVerification' : (context) => const OtpVerificationScreen(),
+          '/emailVerification': (context) => const EmailVerificationScreen(),
           '/accountrecovery' : (context) => const AccountRecoveryPage(),
           '/main' : (context) => const MainScreen(),
         },
